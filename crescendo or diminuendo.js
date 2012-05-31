d829d2261f510a04025837231afbc79f8d7cc6f7
Joachim 'Jojo' Schmitz <jojo@schmitz-digital.de>
2012-05-31 12:23:44 +0200

    Now it works on all selected staves

--- crescendo or diminuendo.js
+++ crescendo or diminuendo.js
@@ -4,11 +4,11 @@
 //
 //  Crescendo and Diminuendo plugin
 //
-//	Creates note velocities for crescendos and diminuendos
-//	Version 0.5 - 2012
+//   Creates note velocities for crescendos and diminuendos
+//   Version 0.6 - 2012
 //
-//	By Tory Gaurnier, 2011
-//	By Joachim Schmitz, 2012
+//   By Tory Gaurnier, 2011
+//   By Joachim Schmitz, 2012
 //
 //  MuseScore: Copyright (C)2008 Werner Schweer and others
 //
@@ -29,77 +29,85 @@ function init() {
 };
 
 function run() {
-	// no score open (MuseScore 2.0+, can't happen earlier)
-	if (typeof curScore === 'undefined')
-		return;
-	// MuseScore version too old
-	else if (mscoreVersion < 10100) {
-		messageBox = new QMessageBox();
-		messageBox.setWindowTitle("Message Box");
-		messageBox.text = "Sorry, but you need MuseScore version 1.1 or higher to use this plugin, please update.";
-		messageBox.exec();
-		return;
-	}
+   // no score open (MuseScore 2.0+, can't happen earlier)
+   if (typeof curScore === 'undefined')
+      return;
+   // MuseScore version too old
+   else if (mscoreVersion < 10100) {
+      messageBox = new QMessageBox();
+      messageBox.setWindowTitle("Message Box");
+      messageBox.text = "Sorry, but you need MuseScore version 1.1 or higher to use this plugin, please update.";
+      messageBox.exec();
+      return;
+   }
 
-	var cursor = new Cursor(curScore);
-	var selectionEnd = new Cursor(curScore);
+   var cursor = new Cursor(curScore);
+   var selectionEnd = new Cursor(curScore);
+   
+   cursor.goToSelectionStart();
+   selectionEnd.goToSelectionEnd();
+   var startStaff = cursor.staff;
+   var endStaff   = selectionEnd.staff;
+   for (var staff = startStaff; staff < endStaff; staff++) {
+      for (var voice = 0; voice < 4; voice++) {
+         var numberOfChords = 0;
+         var startingVelocity = 0;
+         var endingVelocity = 0;
 
-	for (var voice = 0; voice < 4; voice++) {
-		var numberOfChords = 0;
-		var startingVelocity = 0;
-		var endingVelocity = 0;
+         cursor.goToSelectionStart();
+         selectionEnd.goToSelectionEnd();
+         // the above two calls reset voice (to 0) and staff
+         cursor.voice = voice;
+         cursor.staff = staff;
 
-		cursor.goToSelectionStart();
-		selectionEnd.goToSelectionEnd();
-        	// the above two calls reset voice to 0
-		cursor.voice = voice;
+         // Find how many notes/chords are in the selection
+         while (cursor.tick() < selectionEnd.tick()) {
+            if (cursor.isChord()) {
+               numberOfChords++;
+               // Get starting and ending velocities
+               if (startingVelocity == 0)
+                  startingVelocity = cursor.chord().note(0).velocity;
+               endingVelocity = cursor.chord().note(0).velocity;
+            }
+            cursor.next();
+         }
+   
+         // Nothing to do
+         if (numberOfChords == 0)
+            break;
 
-		// Find how many notes/chords are in the selection
-		while (cursor.tick() < selectionEnd.tick()) {
-			if (cursor.isChord()) {
-				numberOfChords++;
-				// Get starting and ending velocities
-				if (startingVelocity == 0)
-					startingVelocity = cursor.chord().note(0).velocity;
-				endingVelocity = cursor.chord().note(0).velocity;
-			}
-			cursor.next();
-		}
-	
-		// Nothing to do
-		if (numberOfChords == 0)
-			return;
+         // Calculate increment value
+         var increment = (endingVelocity - startingVelocity) / numberOfChords;
 
-		// Calculate increment value
-		var increment = (endingVelocity - startingVelocity) / numberOfChords;
-
-		// Set velocity to all notes of all chords
-		cursor.goToSelectionStart();
-        	// the above call resets voice to 0
-        	cursor.voice = voice;
-		for (var c = 1; c <= numberOfChords; c++) {
-			while (!cursor.isChord())
-				cursor.next();
-			if (c != 1 && c != numberOfChords) {
-				for (var n = 0; n < cursor.chord().notes; n++) {
-					cursor.chord().note(n).velocity = startingVelocity + Math.round(increment * c);
-				}
-			}
-			cursor.next();
-		}
-	}
+         // Set velocity to all notes of all chords
+         cursor.goToSelectionStart();
+         // the above call resets voice (to 0) and staff
+         cursor.voice = voice;
+         cursor.staff = staff;
+         for (var c = 1; c <= numberOfChords; c++) {
+            while (!cursor.isChord())
+               cursor.next();
+            if (c != 1 && c != numberOfChords) {
+               for (var n = 0; n < cursor.chord().notes; n++) {
+                  cursor.chord().note(n).velocity = startingVelocity + Math.round(increment * c);
+               } // note loop
+            } 
+            cursor.next();
+         } // chord loop 
+      } // voice loop
+   } // staff loop
 };
 
 function close() {
 };
 
 mscorePlugin = {
-	majorVersion: 1,
-	minorVersion: 1,
-	menu: 'Plugins.Create Crescendo or Diminuendo',
-	init: init,
-	run: run,
-	onClose: close
+   majorVersion: 1,
+   minorVersion: 1,
+   menu: 'Plugins.Create Crescendo or Diminuendo',
+   init: init,
+   run: run,
+   onClose: close
 };
 
 mscorePlugin;
